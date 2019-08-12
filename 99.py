ec02a81344c5a0d5fbd5ed6bb074a68b350e3eb0
Tushar <tushar1994gupta@gmail.com>
2019-08-12 12:47:43 -0300

    Create 99.py
    got 99 problems but this aint one

---
+++ 99.py
@@ -0,0 +1,23 @@
+# a^b = x
+# log(a)(x)=b
+# change base for each log -> log(e)(x)/log(e)log(a) = b
+# log(e)(x) = b * log(e)(a)
+# x - > b * log(e)(a)   [Since every number is of the same power of e , we can ignore it]
+
+
+
+import math
+
+temp = 0
+i = -1
+with open("data.txt",'r') as f:
+  data = f.readlines()
+  for index,line in enumerate(data,1):
+    num,power = list(map(int,line.split(",")))
+    t = power * math.log(num)
+    if t > temp:
+      temp = t
+      i = index
+  
+  print(i)
+  
