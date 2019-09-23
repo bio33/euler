5a57e5df50fee1331b2524b5f2f15d8a6bea9c1b
Tushar <tushar1994gupta@gmail.com>
2019-09-23 14:26:01 -0300

    Rename 8.py to 8_temp.py

---
+++ 8_temp.py
@@ -0,0 +1,16 @@
+from functools import reduce
+with open("data.txt","r") as f:
+  temp = ""
+  x = f.readline().strip()
+  while x:
+    temp += x
+    x = f.readline().strip()
+  max_pro = 1
+  for i in range(0,987):
+    t = [int(x) for x in temp[i:i+13]]
+    print(t)
+    pro_t = reduce(lambda x,y:x*y, t)
+    if pro_t > max_pro:
+      max_pro = pro_t
+  print(max_pro)
+    
